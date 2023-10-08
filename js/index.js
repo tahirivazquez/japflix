e36d30103db5af3e73a9370b05954b2f7cca32d9
tahirivazquez <tahirivazquez56@gmail.com>
2023-10-08 02:12:24 -0300

    arreglos en fetch y funciones

--- js/index.js
+++ js/index.js
@@ -3,33 +3,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnBuscar = document.getElementById("btnBuscar");
     const inputBuscar = document.getElementById("inputBuscar");
 
-    let peliculas = [];
+    let peliculasArray = [];
+    const API_URL = 'https://japceibal.github.io/japflix_api/movies-data.json';
 
-    fetch('https://japceibal.github.io/japflix_api/movies-data.json')
-        .then(response => response.json())
-        .then(data => {
-            peliculas = data;
-        })
-        .catch(error => {
-            console.error("Error al cargar los datos:", error);
-        });
+        async function fetchPelis() {
+            try {
+                const response = await fetch(API_URL);
+        
+                if (response.ok) {
+                    const data = await response.json();
+                    peliculasArray.push(...data);
+                } else {
+                    throw new Error('Error al cargar los datos de películas');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchPelis();
 
     btnBuscar.addEventListener("click", function () {
         lista.innerHTML = "";
 
         const buscarPeli = inputBuscar.value.toLowerCase();
 
-        peliculas.forEach(function (pelicula) {
+        peliculasArray.forEach(function (pelicula) {
             const titulo = pelicula.title.toLowerCase();
             const generos = pelicula.title.toLowerCase();
             const resumen = pelicula.overview.toLowerCase();
             const eslogan = pelicula.tagline.toLowerCase();
 
-            if (titulo.includes(buscarPeli) || generos.includes(buscarPeli) || resumen.includes(buscarPeli) || eslogan.includes(buscarPeli)) {
-                const peliculaLi = document.createElement("li");
-                peliculaLi.className = "list-group-item";
-                peliculaLi.innerHTML = `<h2>${pelicula.title}</h2><p>${pelicula.tagline}</p>`;
-                lista.appendChild(peliculaLi);
+            const mostrarPelis = () => {
+                if (titulo.includes(buscarPeli) || generos.includes(buscarPeli) || resumen.includes(buscarPeli) || eslogan.includes(buscarPeli)) {
+                    const peliculaLi = document.createElement("li");
+                    peliculaLi.className = "list-group-item";
+                    peliculaLi.innerHTML = `<h2>${pelicula.title}</h2><p>${pelicula.tagline}</p>`;
+                    lista.appendChild(peliculaLi);
+                }
             }
         });
     });
